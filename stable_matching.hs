fb8f7185c775d90a24893b37b4fcb11ed54cc1d4
Steven Wu <stevenwu@seas807.wireless-pennnet.upenn.edu>
2013-12-01 17:08:16 -0500

    update the haskell file

--- stable_matching.hs
+++ stable_matching.hs
@@ -35,27 +35,58 @@ morePref (h:tl) a b
   | h == b = b
   | otherwise = morePref tl a b
 
-matchedWomen :: [Matching] -> [Person]
-matchedWomen [] = []
-matchedWomen (h:tl) = (woman h) : (matchedWomen tl)
-  where woman :: Matching -> Person
-        woman (_ , b) = b
+--  find out whether a woman would want to deviate 
+wouldCheat :: [Pref] -> Person -> Matching -> Bool
+wouldCheat prefs  person (m, w) = morePref (prefs !! w) m person == person
+
+
+-- some dude wants to see which girls are in his league
+inTheLeague :: Person -> [Pref] -> [Matching] -> [Person]
+inTheLeague man womenPrefs matchings = map woman list
+  where list = filter (wouldCheat womenPrefs man) matchings
+        woman :: Matching -> Person
+        woman (_, b) = b
+
+
+
+matchedMen :: [Matching] -> [Person]
+matchedMen [] = []
+matchedMen (h:tl) = (man h) : (matchedMen tl)
+  where man :: Matching -> Person
+        man (a , _) = a
+
+unmatchedMen :: [Matching] -> [Person]
+unmatchedMen l = [1..n] \\ ( matchedMen l )
+
+
 
 
 -- a man should look at the current matching and tried to propose
 -- it should be feasible
+feasibleFavorite ::  Person -> [Pref] -> [Pref] -> [Matching] -> Person
+feasibleFavorite man menPrefs womenPrefs matchings = 
+  favorite (menPrefs !! man) list
+  where list = inTheLeague man womenPrefs matchings
+        favorite :: Pref -> [Person] -> Person
+        favorite (h:tl) list = if (elem h list)
+                               then h
+                               else favorite tl list
 
 
 
+propose :: Person -> [Pref] -> [Pref] -> [Matching] -> [Matching]
+propose man menPrefs womenPrefs matchings = newMatching
+  where theOne = feasibleFavorite man menPrefs womenPrefs matchings
+        newMatching = updateMatching matchings
+        updateMatching :: [Matching] -> [Matching]
+        updateMatching ((m,w):tl) = if (w == theOne)
+                                    then ((man, w):tl)
+                                    else (m,w):(updateMatching tl)
+        updateMatching [] = []
 
-feasibleFavorite ::  Pref -> [Matching] -> [Pref] -> Person
-feasibleFavorite = undefined
 
-propose :: Person -> Pref -> [Matching] -> [Matching]
-propose = undefined
-
-
-
+-- this is the function that keeps 
+match :: [Matching] -> [Matching]
 
 
 
